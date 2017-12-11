bd7038c924df3f86f8a6f91b252a9532bedc1fb2
ronaldodoi <arthurpolima@gmail.com>
2017-12-11 19:38:56 -0200

    Transferindo o coin controll para o playerCtrl

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -127,4 +127,12 @@ public class PlayerCtrl : MonoBehaviour {
 		}
 	}
 
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
 }
